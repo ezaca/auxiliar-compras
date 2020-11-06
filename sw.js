62a259f1fe06c0bb952b5a8c63b15abc39f27db2
Eliakim Zacarias <eliakim.zacarias@gmail.com>
2020-11-06 00:40:21 -0300

    Small fixes

--- sw.js
+++ sw.js
@@ -5,14 +5,24 @@ self.addEventListener('install', function(e) {
                 // App
                 '/auxiliar-compras/',
                 '/auxiliar-compras/index.html',
+                '/auxiliar-compras/list.html',
                 '/auxiliar-compras/js/index.js',
                 '/auxiliar-compras/css/style.css',
                 '/auxiliar-compras/LICENSE',
+
+                // Icons8
+                '/auxiliar-compras/icons8/icons8-back-to-36.png',
+                '/auxiliar-compras/icons8/icons8-pencil-26.png',
+                '/auxiliar-compras/icons8/icons8-trash-26.png',
 
                 // Bootstrap
                 '/auxiliar-compras/third-party/bootstrap4/css/bootstrap.min.css',
                 '/auxiliar-compras/third-party/bootstrap4/js/bootstrap.min.js',
                 '/auxiliar-compras/third-party/bootstrap4/LICENSE',
+
+                // VueJS
+                '/auxiliar-compras/third-party/vuejs/LICENSE',
+                '/auxiliar-compras/third-party/vuejs/vue.min.js',
             ]);
         })
     );
